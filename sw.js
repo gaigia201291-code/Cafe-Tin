70a6bc64d10dea7127c84ee746579ffe1ce8cbc0
gaigia201291-code <gaigia201291@gmail.com>
2026-08-14 15:11:03 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "quaytin-offline-v2";
+const CACHE = "quaytin-offline-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
